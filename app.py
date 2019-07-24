f3ab51fed6fc8d814578411cf722a9160e0c2325
HKG <hkg902@gmail.com>
2019-07-24 10:42:33 +0200

    Update *slow* flask with *fast* sanic

--- app.py
+++ app.py
@@ -1,40 +1,56 @@
-import json
-import os
-from flask import Flask, redirect, url_for, render_template
-from flask_dance.contrib.discord import make_discord_blueprint, discord
-from discord_entites import User, Guild
-from typing import List
+from json import load as load_json
+import aiohttp
+import aiohttp.web_exceptions
+from sanic import Sanic
+from sanic.request import Request
+from sanic.response import text, json, HTTPResponse
+from sanic_oauth.blueprint import oauth_blueprint, login_required
+from sanic_session import InMemorySessionInterface
+from discord_entites import User, Guild, get_user, get_guilds
 
 with open("config.json") as f:
-    config = json.load(f)
+    config = load_json(f)
 
-app = Flask(__name__)
-app.secret_key = config["app_secret"]
+app = Sanic(__name__)
+app.blueprint(oauth_blueprint)
+app.session_interface = InMemorySessionInterface()
+app.config.OAUTH_PROVIDER = "sanic_oauth.providers.DiscordClient"
+app.config.OAUTH_REDIRECT_URI = "http://127.0.0.1:5000/oauth"
+app.config.OAUTH_SCOPE = 'identify guilds'
+app.config.OAUTH_CLIENT_ID = config["app_id"]
+app.config.OAUTH_CLIENT_SECRET = config["app_secret"]
+# TODO: get a module and push above to a function
 
-# so authlib won't be shouting at us for insecure oauth2.0
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
-blueprint = make_discord_blueprint(
-    client_id=config["app_id"],
-    client_secret=config["app_secret"],
-    scope=["identify", "guilds"]
-)
+@app.listener("before_server_start")
+async def init_aiohttp_session(sanic_app, _loop):
+    sanic_app.async_session = aiohttp.ClientSession()
 
-app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.listener("after_server_stop")
+async def close_aiohttp_session(sanic_app, _loop):
+    await sanic_app.async_session.close()
+
+
+@app.middleware("request")
+async def add_session_to_request(request: Request):
+    await request.app.session_interface.open(request)
+
+
+@app.middleware("response")
+async def save_session(request: Request, response: HTTPResponse):
+    await request.app.session_interface.save(request, response)
 
 
 @app.route("/")
-def index():
-    if not discord.authorized:
-        return redirect(url_for("discord.login"))
-
-    user_resp = discord.get("/api/v6/users/@me")
-    user = User.from_dict(json.loads(user_resp.content))
-    guilds_resp = discord.get("/api/v6/users/@me/guilds")
-    guilds = map(Guild.from_dict, json.loads(guilds_resp.content))
-
-    return render_template("index.html", user=user, guilds=guilds)
-
+@login_required(add_user_info=False)
+async def index(request: Request) -> HTTPResponse:
+    provider = request.app.oauth_factory(**{'access_token': request['session']['token']})
+    user = await get_user(provider)
+    guilds = await get_guilds(provider)
+    return text(f"uwu hi there {user.username}#{user.discriminator}!"
+                f"\ni was uwu waiting for you uwu \nyou're in {len(list(guilds))} guilds!")
 
 if __name__ == "__main__":
     app.run(port=5000)
+    print("owo")
