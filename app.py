495d67c5d47077e485da3ace4a587a713bc81a73
HKG <hkg902@gmail.com>
2019-07-25 02:58:53 +0200

    Use high-level ThreadPoolExecutor instead of bare Threads.

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import asyncio
-from typing import Iterator, Coroutine
-from collections import defaultdict
-import threading
-
-import aiohttp
+from concurrent.futures import Executor
+from typing import Iterator
+import concurrent.futures
 
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import text, json, HTTPResponse
-from sanic_oauth.blueprint import oauth_blueprint, login_required
+from sanic_oauth.blueprint import login_required
 from sanic_oauth.providers import DiscordClient
 from sanic_jinja2 import SanicJinja2
 
@@ -39,10 +37,15 @@ async def index(request: Request) -> HTTPResponse:
 
 
 if __name__ == "__main__":
-    sanic_thread: threading.Thread = threading.Thread(target=app.run, kwargs={"port": 5000})
-
     def run_bot() -> None:
         bot.run(config.bot_token)
-    bot_thread: threading.Thread = threading.Thread(target=run_bot)
-    sanic_thread.start()
-    bot_thread.start()
+
+    def run_sanic() -> None:
+        app.run(port=5000)
+
+    executor: Executor
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(bot.run, config.bot_token)
+        executor.submit(app.run, port=5000)
+
+
