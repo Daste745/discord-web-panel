c81b87fbccbe4dfe9194881958baa5fa06d0f783
Stefankar1000 <stefankar1000@gmail.com>
2019-07-26 04:08:55 +0200

    Add a bot invite button.

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,7 @@
             element.innerText = "";
         }
     };
+
     const selectAllRoles = () => {
         if (!Sortable.utils.select) return;
         const elements = Array.from(document.getElementById("section-sortable").children);
@@ -25,18 +26,28 @@
             })
         );
     };
+
     const selectGuild = (event) => {
         for (let element of document.querySelectorAll("#server-list > label")) {
             element.classList.remove("selected")
         }
         event.target.parentElement.classList.add("selected")
     };
+
+    const redirectToBotInvite = () => {
+        window.open(
+            "https://discordapp.com/oauth2/authorize?client_id=603111310770831360&scope=bot&permissions=268438528",
+            "_blank"
+        )
+    };
+
     document.addEventListener("DOMContentLoaded", (_event) => {
         createSortables();
         setColorsInRoles();
         Array.from(document.querySelectorAll("#server-list > label > input"))
             .forEach((x) => x.addEventListener("click", selectGuild));
         document.getElementById("select-all-roles-button").addEventListener("click", selectAllRoles);
+        document.getElementById("bot-invite-button").addEventListener("click", redirectToBotInvite);
     });
 
 }());
